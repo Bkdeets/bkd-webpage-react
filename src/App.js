19f0047864b7a6e2ce6b43da70200f51768ab84b
Britton <brittonkdeets@gmail.com>
2020-12-04 09:00:06 -0500

    update style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main/Main';
 import Portfolio from './components/Portfolio/Portfolio';
+import Current from './components/Current/Current';
 
 function App() {
+  const [activeItem, setActiveItem] = useState(null);
+    const handleMenuItemChange = (item) => {
+        setActiveItem(item)
+    }
+
   return (
     <div className="App">
       <Main />
-      <Portfolio />
+      <Portfolio handleMenuItemChange={handleMenuItemChange} />
+      <div className="mt-4 w-full">
+        {activeItem}
+      </div>
     </div>
   );
 }
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import '../../tailwind.output.css';
 
 import Pinpoint from '../Pinpoint/Pinpoint';
@@ -6,16 +6,35 @@ import Quant from '../Quant/Quant';
 import Current from '../Current/Current';
 import Education from '../Education/Education';
 import Interests from '../Interests/Interests';
+import MenuItem from '../MenuItem/MenuItem';
 
-const Portfolio = () => {
+const Portfolio = (props) => {
+    const ITEM_MAP = {
+        "nCino" : <Current></Current>,
+        "Pinpoint Surf": <Pinpoint></Pinpoint>,
+        "Quantitative Investing": <Quant></Quant>,
+        "Education": <Education></Education>,
+        "Interests": <Interests></Interests>
+    }
+    const [activeItem, setActiveItem] = useState(null);
+    const [activeItemName, setActiveItemName] = useState("");
+    const handleMenuItemClick = (name) => {
+        setActiveItem(ITEM_MAP[name]);
+        setActiveItemName(name);
+    }
+    
+
+    useEffect(() => {
+        props.handleMenuItemChange(activeItem);
+    }, [activeItem]);
     return (
-        <Fragment>
-            <Current />
-            <Pinpoint />
-            <Quant />
-            <Education />
-            <Interests />
-        </Fragment>
+        <div className="flex px-16 pt-4 text-left mx-auto flex-wrap sm-max:px-2 sm-max:pt-0">
+            <MenuItem isActive={"nCino" == activeItemName} name="nCino" hoverColor="bg-yellow-200" handleMenuItemClick={handleMenuItemClick}></MenuItem>
+            <MenuItem isActive={"Pinpoint Surf" == activeItemName} name="Pinpoint Surf" hoverColor="bg-blue-200" handleMenuItemClick={handleMenuItemClick}></MenuItem>
+            <MenuItem isActive={"Quantitative Investing" == activeItemName} name="Quantitative Investing" hoverColor="bg-teal-200" handleMenuItemClick={handleMenuItemClick}></MenuItem>
+            <MenuItem isActive={"Education" == activeItemName} name="Education" hoverColor="bg-teal-400" handleMenuItemClick={handleMenuItemClick}></MenuItem>
+            <MenuItem isActive={"Interests" == activeItemName} name="Interests" hoverColor="bg-purple-200" handleMenuItemClick={handleMenuItemClick}></MenuItem>
+        </div>
     );
 }
 
